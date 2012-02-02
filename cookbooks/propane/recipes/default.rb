d8fef0241c84f6a347eeaf1a0000a71e0ee313ca
Glenn Gillen <me@glenngillen.com>
2012-02-02 10:59:48 -0800

    Adding propane to dev dependencies.

---
+++ cookbooks/propane/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: propane
+# Recipe:: default
+#
+# Copyright 2012, Glenn Gillen
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+execute "install propane" do
+  command "curl -sfL -o /tmp/propane.zip http://propaneapp.com/appcast/Propane.zip && unzip -d /Applications"
+  cwd "/Applications"
+  not_if { File.exist? "/Applications/Propane.app" }
+end
