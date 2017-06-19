92556c0def5eaebbd5ba652c154b72fd8d7e735d
Glenn Gillen <me@glenngillen.com>
2017-06-19 23:41:48 +1000

    Remove last vestiges of Chef integration.

--- cookbooks/sshkeys/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Glenn Gillen"
-maintainer_email "me@glenngillen.com"
-license          "All rights reserved"
-description      "Installs/Configures sshkeys"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
